269cae3aa82ff153952acaef45c07710b26fe733
0xGUI <29775338+0xGUI@users.noreply.github.com>
2021-03-13 23:09:35 +0000

    update hello world

--- hello-world/v1/hello.go
+++ hello-world/v1/hello.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+func Hello() string{
+  return "Not and Hello lol"
+}
+
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println(Hello())
 }
--- hello-world/v2/hello.go
+++ hello-world/v2/hello.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // Hello returns a greeting.
 func Hello() string {
-	return "Hello, world"
+	return "No Hello, world"
 }
 
 func main() {
